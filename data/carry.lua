09e36d0da5b66f86ab5b269d7e0e34a4c43ec3af
FjamZoo <34508112+FjamZoo@users.noreply.github.com>
2024-03-02 20:52:17 +0100

    Feat(carry): Renewed scripts default config

--- data/carry.lua
+++ data/carry.lua
@@ -1,4 +1,6 @@
 return {
+
+    -- Used in Renewed-Fuel
     oilbarrel = {
         model = `prop_barrel_exp_01a`,
         pos = vec3(0.01, -0.27, 0.27),
@@ -8,6 +10,22 @@ return {
         anim = 'idle',
         disableKeys = {
             disableSprint = true,
+            disableJump = true,
+            disableAttack = true,
+            disableVehicle = true
+        }
+    },
+
+    -- Used in Renewed-Garbagejob
+    clothing = {
+        model = `prop_cs_rub_binbag_01`,
+        pos = vec3(0.0, 0.0, 0.0),
+        rot = vec3(180.0, 200.0, 80.0),
+        bone = 28422,
+        dict = 'anim@heists@narcotics@trash',
+        anim = 'walk',
+        disableKeys = {
+            disableSprint = false,
             disableJump = true,
             disableAttack = true,
             disableVehicle = true
